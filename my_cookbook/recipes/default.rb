e124e962990fca0e21440d613850fc0fdcaa5916
joesoh-star <joe.soh@thestar.com.my>
2014-12-03 11:33:17 +0800

    my_cookbook

--- my_cookbook/recipes/default.rb
+++ my_cookbook/recipes/default.rb
@@ -6,3 +6,8 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+node[:deploy].each do |application, deploy|
+  app_root = "#{deploy[:deploy_to]}/current"
+  execute "chmod -R g+rw #{app_root}" do
+  end
+end
