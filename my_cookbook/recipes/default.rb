050975d9f408bce0e92516e23ebd8a1b4ea8cc8b
joesoh-star <joe.soh@thestar.com.my>
2014-12-09 10:37:09 +0800

    Change wp-content/cache and uploads group permission to write-v2

--- my_cookbook/recipes/default.rb
+++ my_cookbook/recipes/default.rb
@@ -7,11 +7,11 @@
 # All rights reserved - Do Not Redistribute
 #
 node[:deploy].each do |application, deploy|
-  app_root = "#{deploy[:deploy_to]}/current/cache"
+  app_root = "#{deploy[:deploy_to]}/current/wp-content/cache"
   execute "chmod -R g+rw #{app_root}" do
   end
 #
-  app_root = "#{deploy[:deploy_to]}/current/uploads"
+  app_root = "#{deploy[:deploy_to]}/current/wp-content/uploads"
   execute "chmod -R g+rw #{app_root}" do
   end
 end
