9230d5e10721957c2c798022ab9296703276582c
joesoh-star <joe.soh@thestar.com.my>
2015-01-05 16:37:22 +0800

    all s3 file permission

--- my_deployfilterfile/recipes/default.rb
+++ my_deployfilterfile/recipes/default.rb
@@ -13,7 +13,7 @@ aws_s3_file "#{deploy[:deploy_to]}/current/wp-content/uploads/filters/blacklist.
   bucket "dsc.propwall.com"
   remote_path "filtering/blacklist.txt"
   aws_access_key_id node['my_deployfilterfile'][:custom_access_key]
-  aws_secret_access_key node['my_deployfilterfile'][:custom_secret_key]a
+  aws_secret_access_key node['my_deployfilterfile'][:custom_secret_key]
   owner 'deploy'
   group 'www-data'
   mode '0644'
