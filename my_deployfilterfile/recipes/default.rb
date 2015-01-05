59d5c450dbe929ba7912406b7dea33b418f92112
joesoh-star <joe.soh@thestar.com.my>
2015-01-05 11:25:47 +0800

    add []

--- my_deployfilterfile/recipes/default.rb
+++ my_deployfilterfile/recipes/default.rb
@@ -4,8 +4,8 @@ node[:deploy].each do |application, deploy|
 aws_s3_file "#{deploy[:deploy_to]}/current/wp-content/uploads/filters/blacklist.txt" do
   bucket "test-dsc.propwall.com"
   remote_path "filtering/blacklist.txt"
-  aws_access_key_id node[:custom_access_key]
-  aws_secret_access_key node[:custom_secret_key]
+  aws_access_key_id node['my_deployfilterfile'][:custom_access_key]
+  aws_secret_access_key node['my_deployfilterfile'][:custom_secret_key]
 end
 
 
