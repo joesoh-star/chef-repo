ab7e22bb3c53bd023c2c969780b1f89d87b81350
joesoh-star <joe.soh@thestar.com.my>
2015-01-05 11:46:31 +0800

    remove test-

--- my_deployfilterfile/recipes/default.rb
+++ my_deployfilterfile/recipes/default.rb
@@ -10,7 +10,7 @@ action :create
 end
 
 aws_s3_file "#{deploy[:deploy_to]}/current/wp-content/uploads/filters/blacklist.txt" do
-  bucket "test-dsc.propwall.com"
+  bucket "dsc.propwall.com"
   remote_path "filtering/blacklist.txt"
   aws_access_key_id node['my_deployfilterfile'][:custom_access_key]
   aws_secret_access_key node['my_deployfilterfile'][:custom_secret_key]
@@ -18,14 +18,14 @@ end
 
 
 aws_s3_file "#{deploy[:deploy_to]}/current/wp-content/uploads/filters/spam.txt" do
-  bucket "test-dsc.propwall.com"
+  bucket "dsc.propwall.com"
   remote_path "filtering/spam.txt"
    aws_access_key_id node['my_deployfilterfile'][:custom_access_key]
   aws_secret_access_key node['my_deployfilterfile'][:custom_secret_key]
 end
 
 aws_s3_file "#{deploy[:deploy_to]}/current/wp-content/uploads/filters/spammer.txt" do
-  bucket "test-dsc.propwall.com"
+  bucket "dsc.propwall.com"
   remote_path "filtering/spammer.txt"
   aws_access_key_id node['my_deployfilterfile'][:custom_access_key]
   aws_secret_access_key node['my_deployfilterfile'][:custom_secret_key]
