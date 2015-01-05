6c608bf215b9301541468f6e8e773c616f8a4249
joesoh-star <joe.soh@thestar.com.my>
2015-01-05 10:34:25 +0800

    change to test bucket

--- my_deployfilterfile/recipes/default.rb
+++ my_deployfilterfile/recipes/default.rb
@@ -2,7 +2,7 @@ include_recipe 'aws'
 
 node[:deploy].each do |application, deploy|
 aws_s3_file "#{deploy[:deploy_to]}/current/wp-content/uploads/filters/blacklist.txt" do
-  bucket "dsc.propwall.com"
+  bucket "test-dsc.propwall.com"
   remote_path "filtering/blacklist.txt"
   aws_access_key_id node[:custom_access_key]
   aws_secret_access_key node[:custom_secret_key]
@@ -10,17 +10,17 @@ end
 
 
 aws_s3_file "#{deploy[:deploy_to]}/current/wp-content/uploads/filters/spam.txt" do
-  bucket "dsc.propwall.com"
+  bucket "test-dsc.propwall.com"
   remote_path "filtering/spam.txt"
   aws_access_key_id node[:custom_access_key]
   aws_secret_access_key node[:custom_secret_key]
 end
 
 aws_s3_file "#{deploy[:deploy_to]}/current/wp-content/uploads/filters/spammer.txt" do
-  bucket "dsc.propwall.com"
+  bucket "test-dsc.propwall.com"
   remote_path "filtering/spammer.txt"
-#  aws_access_key_id node[:custom_access_key]
-#  aws_secret_access_key node[:custom_secret_key]
+  aws_access_key_id node[:custom_access_key]
+  aws_secret_access_key node[:custom_secret_key]
 end
 
 end
