c6fabde8079e22e66419dd66284abc47b80949c1
joesoh-star <joe.soh@thestar.com.my>
2015-01-05 16:33:08 +0800

    all s3 file permission

--- my_deployfilterfile/recipes/default.rb
+++ my_deployfilterfile/recipes/default.rb
@@ -3,7 +3,7 @@ include_recipe 'aws'
 node[:deploy].each do |application, deploy|
 
 directory "#{deploy[:deploy_to]}/current/wp-content/uploads/filters" do
-owner 'www-data'
+owner 'deploy'
 group 'www-data'
 mode '0644'
 action :create
@@ -13,15 +13,21 @@ aws_s3_file "#{deploy[:deploy_to]}/current/wp-content/uploads/filters/blacklist.
   bucket "dsc.propwall.com"
   remote_path "filtering/blacklist.txt"
   aws_access_key_id node['my_deployfilterfile'][:custom_access_key]
-  aws_secret_access_key node['my_deployfilterfile'][:custom_secret_key]
+  aws_secret_access_key node['my_deployfilterfile'][:custom_secret_key]a
+  owner 'deploy'
+  group 'www-data'
+  mode '0644'
 end
 
 
 aws_s3_file "#{deploy[:deploy_to]}/current/wp-content/uploads/filters/spam.txt" do
   bucket "dsc.propwall.com"
   remote_path "filtering/spam.txt"
-   aws_access_key_id node['my_deployfilterfile'][:custom_access_key]
+  aws_access_key_id node['my_deployfilterfile'][:custom_access_key]
   aws_secret_access_key node['my_deployfilterfile'][:custom_secret_key]
+  owner 'deploy'
+  group 'www-data'
+  mode '0644'
 end
 
 aws_s3_file "#{deploy[:deploy_to]}/current/wp-content/uploads/filters/spammer.txt" do
@@ -29,6 +35,9 @@ aws_s3_file "#{deploy[:deploy_to]}/current/wp-content/uploads/filters/spammer.tx
   remote_path "filtering/spammer.txt"
   aws_access_key_id node['my_deployfilterfile'][:custom_access_key]
   aws_secret_access_key node['my_deployfilterfile'][:custom_secret_key]
+  owner 'deploy'
+  group 'www-data'
+  mode '0644'
 end
 
 end
