7af66232f9178d39472fec09545db0ab5b85b361
joesoh-star <joe.soh@thestar.com.my>
2015-01-05 11:38:58 +0800

    add []

--- my_deployfilterfile/recipes/default.rb
+++ my_deployfilterfile/recipes/default.rb
@@ -1,6 +1,14 @@
 include_recipe 'aws'
 
 node[:deploy].each do |application, deploy|
+
+directory "#{deploy[:deploy_to]}/current/wp-content/uploads/filters" do
+owner 'www-data'
+group 'www-data'
+mode '0644'
+action :create
+end
+
 aws_s3_file "#{deploy[:deploy_to]}/current/wp-content/uploads/filters/blacklist.txt" do
   bucket "test-dsc.propwall.com"
   remote_path "filtering/blacklist.txt"
@@ -12,15 +20,15 @@ end
 aws_s3_file "#{deploy[:deploy_to]}/current/wp-content/uploads/filters/spam.txt" do
   bucket "test-dsc.propwall.com"
   remote_path "filtering/spam.txt"
-  aws_access_key_id node[:custom_access_key]
-  aws_secret_access_key node[:custom_secret_key]
+   aws_access_key_id node['my_deployfilterfile'][:custom_access_key]
+  aws_secret_access_key node['my_deployfilterfile'][:custom_secret_key]
 end
 
 aws_s3_file "#{deploy[:deploy_to]}/current/wp-content/uploads/filters/spammer.txt" do
   bucket "test-dsc.propwall.com"
   remote_path "filtering/spammer.txt"
-  aws_access_key_id node[:custom_access_key]
-  aws_secret_access_key node[:custom_secret_key]
+  aws_access_key_id node['my_deployfilterfile'][:custom_access_key]
+  aws_secret_access_key node['my_deployfilterfile'][:custom_secret_key]
 end
 
 end
