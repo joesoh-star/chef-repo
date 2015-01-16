752cc2d2f238f3762520ee32b7d29d3893a5da09
joesoh-star <joe.soh@thestar.com.my>
2015-01-16 16:42:19 +0800

    run composer

--- my_cookbook/recipes/default.rb
+++ my_cookbook/recipes/default.rb
@@ -53,11 +53,15 @@ node[:deploy].each do |application, deploy|
 #  end
 
 #install project vendors
-composer_project "#{deploy[:deploy_to]}/current" do
-    dev false
-    quiet true
-    prefer_dist false
-    action :install
+#composer_project "#{deploy[:deploy_to]}/current" do
+#    dev false
+#    quiet true
+#    prefer_dist false
+#    action :install
+#end
+execute "run-composer" do
+ command "composer install --no-dev --prefer-dist "
+ action :nothing
 end
 
 end
