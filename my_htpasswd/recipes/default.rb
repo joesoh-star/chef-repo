ca2e5aee31eb3c04ea7f8c2f644784f7540598b0
joesoh-star <joe.soh@thestar.com.my>
2015-05-06 14:44:34 +0800

    add my_cookbook_classifieds

--- my_htpasswd/recipes/default.rb
+++ my_htpasswd/recipes/default.rb
@@ -7,17 +7,3 @@
 # All rights reserved - Do Not Redistribute
 #
 include_recipe "htpasswd::default"
-
-directory "/etc/htpasswd/" do
- owner 'root'
- group 'root'
- mode '0755'
- action :create
-end
-
-htpasswd "/etc/htpasswd/.htpasswd" do
- user "foo"
- password "bar"
- type "sha1"
- action :overwrite
-end
