73ea0f5740bd548324e2f5812f77ba05ebcb4ec2
joesoh-star <joe.soh@thestar.com.my>
2015-05-06 14:48:44 +0800

    add my_cookbook_classifieds-v1

---
+++ my_cookbook_classifieds/recipes/default.rb
@@ -0,0 +1,20 @@
+##
+# Cookbook Name:: my_cookbook
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+node[:deploy].each do |application, deploy|
+
+  app_root = "/srv/www/classifieds_carsifu/current/carsifu-v2/storage/logs"
+  directory app_root do
+    owner 'deploy'
+    group 'www-data'
+    mode '0775' 
+    action :create 
+  end
+
+
+end
