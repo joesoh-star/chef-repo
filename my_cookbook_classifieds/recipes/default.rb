cdbc3a8377f70bb5ed449199d78f2f556e3e75f1
joesoh-star <joe.soh@thestar.com.my>
2015-05-12 15:34:37 +0800

    Change file permission

--- my_cookbook_classifieds/recipes/default.rb
+++ my_cookbook_classifieds/recipes/default.rb
@@ -29,38 +29,4 @@ execute "chmo-775" do
   action :run
 end
 
-  app_root = "/srv/www/classifieds_carsifu/current/carsifu-v2/storage/framework"
-  directory app_root do
-    owner 'deploy'
-    group 'www-data'
-    mode '0775' 
-    recursive true
-    action :create 
-  end
-
-  app_root = "/srv/www/classifieds_carsifu/current/carsifu-v2/storage/framework/views"
-  directory app_root do
-    owner 'deploy'
-    group 'www-data'
-    mode '0775' 
-    recursive true
-    action :create 
-  end
-  
-  app_root = "/srv/www/classifieds_carsifu/current/carsifu-v2/storage/framework/sessions"
-  directory app_root do
-    owner 'deploy'
-    group 'www-data'
-    mode '0775' 
-    recursive true
-    action :create 
-  end
-  app_root = "/srv/www/carsifu/current/automania-v2/wp-content/uploads"
-  directory app_root do
-    owner 'deploy'
-    group 'www-data'
-    mode '0775' 
-    action :create 
-  end
-
 end
