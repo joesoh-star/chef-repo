846a53cd30cff87dc3904e5b36b27c1cc6eb2997
joesoh-star <joe.soh@thestar.com.my>
2015-05-14 17:11:29 +0800

    add mypropwallconnect

---
+++ my_cookbook_propwallconnect/recipes/default.rb
@@ -0,0 +1,39 @@
+##
+# Cookbook Name:: my_cookbook
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+node[:deploy].each do |application, deploy|
+
+  app_root = "#{deploy[:deploy_to]}/current/storage"
+  directory app_root do
+    owner 'deploy'
+    group 'www-data'
+    mode '0775' 
+    recursive true
+  end
+
+execute "chmo-775" do
+  command "chmod -R 775  #{deploy[:deploy_to]}/current/storage/framework; chmod 775  #{deploy[:deploy_to]}/current/storage/logs; chmod a+x  #{deploy[:deploy_to]}/current/vendor/monolog/monolog/src/Monolog/Handler"
+  action :run
+end
+
+execute "php5enmod mcrypt" do
+  command "php5enmod mcrypt; service apache2 restart"
+  action :run
+end
+
+# add htpasswd user.
+include_recipe "htpasswd::default"
+
+htpasswd "/etc/apache2/.htpassword" do
+  user node[:apache][:htpasswd_user] 
+  password node[:apache][:htpasswd_passwd] 
+  type "sha1"
+  action :overwrite
+end
+
+end
