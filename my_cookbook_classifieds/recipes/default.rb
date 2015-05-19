6b6ecb51ffff73733e53528fb77470420c6393f5
joesoh-star <joe.soh@thestar.com.my>
2015-05-19 14:57:02 +0800

    change password to passwd

--- my_cookbook_classifieds/recipes/default.rb
+++ my_cookbook_classifieds/recipes/default.rb
@@ -29,7 +29,7 @@ end
 # add htpasswd user.
 include_recipe "htpasswd::default"
 
-htpasswd "/etc/apache2/.htpassword" do
+htpasswd "/etc/apache2/.htpasswd" do
   user node[:apache][:htpasswd_user] 
   password node[:apache][:htpasswd_passwd] 
   type "sha1"
