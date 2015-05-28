98ad7e99fd89ef8a6bafcf7e738e0b309bc166ac
joesoh-star <joe.soh@thestar.com.my>
2015-05-28 16:39:30 +0800

    remove mod proxy

--- my_cookbook_propwallconnect/recipes/default.rb
+++ my_cookbook_propwallconnect/recipes/default.rb
@@ -8,10 +8,10 @@
 #
 node[:deploy].each do |application, deploy|
 
-execute "php5enmod mcrypt" do
-  command "php5enmod mcrypt; composer update; service apache2 restart"
-  action :run
-end
+#execute "php5enmod mcrypt" do
+#  command "php5enmod mcrypt; composer update; service apache2 restart"
+#  action :run
+#end
 
 # add htpasswd user.
 include_recipe "htpasswd::default"
