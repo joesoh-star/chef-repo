e5c25bc2540b04cf8babb76a7d42a2d21db5c4a0
joesoh-star <joe.soh@thestar.com.my>
2015-08-19 11:49:27 +0800

    add cloudwatch

--- logs/attributes/default.rb
+++ logs/attributes/default.rb
@@ -1,1 +1,3 @@
 default[:cwlogs][:logfile] = '/var/log/aws/opsworks/opsworks-agent.log'
+default[:cwlogs][:apacheaccesslog] = '/var/log/aws/opsworks/opsworks-agent.log'
+default[:cwlogs][:apacheerrorlog] = '/var/log/aws/opsworks/opsworks-agent.log'
