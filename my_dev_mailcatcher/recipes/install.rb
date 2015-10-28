ade6afcd386cee41ee058919e699614c65198229
star7kien <che01@PEI-0013.internal.pei-star.com>
2015-10-28 14:31:09 +0800

    Added my_dev_mailcatcher For dev environment

---
+++ my_dev_mailcatcher/recipes/install.rb
@@ -0,0 +1,12 @@
+# To install mailcatcher for development environment
+# For Development Environment Only
+
+execute "intall-mailcatcher" do
+	command "gem install mailcatcher"
+	action :run
+end
+
+execute "add param to php.ini" do
+	command %Q[echo "sendmail_path = /usr/bin/env catchmail -f admin@propwall.com">> etc/php5/apache2/php.ini]
+	action :run
+end
