03805d62a1a1802252a79a91275a28b35d5c002d
star7kien <che01@PEI-0013.internal.pei-star.com>
2016-01-07 17:55:41 +0800

    Modified: my_supervisord/recipes/deploy.rb
    *Fixed syntax error "supervisorctl", changed :domains to :domains.first

--- my_supervisord/recipes/deploy.rb
+++ my_supervisord/recipes/deploy.rb
@@ -3,22 +3,22 @@
 
 node[:deploy].each do |application, deploy|
 	execute "create-conf" do
-		command %Q[touch /etc/supervisor/conf.d/#{deploy[:domains]}.conf]
+		command %Q[touch /etc/supervisor/conf.d/#{deploy[:domains].first}.conf]
 		action :run
 	end
 
 	execute "insert-conf" do
-		command %Q[echo "[program:#{deploy[:domains]}]\n    command=php #{deploy[:deploy_to]}/current/artisan queue:work --sleep=3 --tries=3 --daemon\n    autostart=true\n    autorestart=true\n    stderr_logfile=/var/log/supervisor/#{deploy[:domains]}.err.log\n    stdout_logfile=/var/log/supervisor/#{deploy[:domains]}.out.log\n    user=www-data\n" >> /etc/supervisor/conf.d/#{deploy[:domains]}.conf]
+		command %Q[echo "[program:#{deploy[:domains].first}]\n    command=php #{deploy[:deploy_to]}/current/artisan queue:work --sleep=3 --tries=3 --daemon\n    autostart=true\n    autorestart=true\n    stderr_logfile=/var/log/supervisor/#{deploy[:domains].first}.err.log\n    stdout_logfile=/var/log/supervisor/#{deploy[:domains].first}.out.log\n    user=www-data\n" >> /etc/supervisor/conf.d/#{deploy[:domains].first}.conf]
 		action :run
 	end
 	
 	execute "supervisor-reread-config" do
-		command "supervisor reread"
+		command "supervisorctl reread"
 		action :run
 	end
 	
 	execute "supervisor-update-config" do
-		command "supervisor update"
+		command "supervisorctl update"
 		action :run
 	end
 	
