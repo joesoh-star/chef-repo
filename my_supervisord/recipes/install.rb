c3ffa9105c285cfa46d24412652d76a0c1a8abb5
star7kien <che01@PEI-0013.internal.pei-star.com>
2016-03-14 15:24:02 +0800

    Modified: my_supervisord/recipes/install.rb #Included python-pip and superlance
    Added: my_supervisord/templates/supervisord.conf.erb
    #Variable: Email Address

--- my_supervisord/recipes/install.rb
+++ my_supervisord/recipes/install.rb
@@ -4,3 +4,26 @@ execute "intall-supervisor" do
 	command "apt-get install -y supervisor"
 	action :run
 end
+
+execute "intall-sendmail" do
+	command "apt-get install -y sendmail"
+	action :run
+end
+
+execute "intall-python-pip" do
+	command "apt-get install -y python-pip"
+	action :run
+end
+
+execute "intall-superlance" do
+	command "pip install superlance"
+	action :run
+end
+
+template "/etc/supervisor/supervisord.conf" do
+	source "supervisord.conf.erb"
+	variables(
+	email_address => node[:supervisord][:email_address]
+	)
+	action :overwrite
+end
