79f29faaa8c040ac36caaff8dbe1e232f02841ee
star7kien <che01@PEI-0013.internal.pei-star.com>
2016-04-14 16:21:16 +0800

    Modified: my_logentries/recipes/deploy.rb
    *Fixed syntax error

--- my_logentries/recipes/deploy.rb
+++ my_logentries/recipes/deploy.rb
@@ -4,16 +4,17 @@
 
 node[:deploy].each do |application, deploy|
 
-template "/etc/rsyslog.d/logentries.conf" do
-	source "logentries.conf.erb"
-	variables(
-	:lelogfile => node['my_logentries']['lelogfile'],
-	:letoken => node['my_logentries']['letoken']
-	)
-	action :create
-end
+	template "/etc/rsyslog.d/logentries.conf" do
+		source "logentries.conf.erb"
+		variables(
+		:lelogfile => node['my_logentries']['lelogfile'],
+		:letoken => node['my_logentries']['letoken']
+		)
+		action :create
+	end
 
-execute "restart-rsyslog" do
-	command "service rsyslog restart"
-	action :run
+	execute "restart-rsyslog" do
+		command "service rsyslog restart"
+		action :run
+	end
 end
