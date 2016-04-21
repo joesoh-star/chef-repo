6d7202f07ab0af481332630e284c06b706ff7a88
star7kien <che01@PEI-0013.internal.pei-star.com>
2016-04-21 14:12:42 +0800

    modified:   my_logentries/recipes/deploy.rb modified:   my_logentries/templates/default/logentries.conf.erb
    *Added LE Token based log for debug, error, and auth log

--- my_logentries/recipes/deploy.rb
+++ my_logentries/recipes/deploy.rb
@@ -7,8 +7,14 @@ node[:deploy].each do |application, deploy|
 	template "/etc/rsyslog.d/logentries.conf" do
 		source "logentries.conf.erb"
 		variables(
-		:lelogfile => node['my_logentries']['lelogfile'],
-		:letoken => node['my_logentries']['letoken']
+		:le_access => node['my_logentries']['le_access'],
+		:le_access_token => node['my_logentries']['le_access_token'],
+		:le_error => node['my_logentries']['le_error'],
+		:le_error_token => node['my_logentries']['le_error_token'],
+		:le_auth => node['my_logentries']['le_auth'],
+		:le_auth_token => node['my_logentries']['le_auth_token'],
+		:le_debug => node['my_logentries']['le_debug'],
+		:le_debug_token => node['my_logentries']['le_debug_token']		
 		)
 		action :create
 	end
