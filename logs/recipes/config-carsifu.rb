7afd754f2ffe6f3817c1ec551f43de30e9a8596d
star7kien <che01@PEI-0013.internal.pei-star.com>
2016-05-25 14:45:28 +0800

    modified:   logs/recipes/config-carsifu.rb modified:   logs/templates/default/cw-csv3-devops.cfg.erb
    *Changed from hard coded file path and log stream name to variable.

--- logs/recipes/config-carsifu.rb
+++ logs/recipes/config-carsifu.rb
@@ -9,6 +9,18 @@ case node[:opsworks][:stack][:name]
 		template "/tmp/cwlogs.cfg" do
 			cookbook "logs"
 			source "cw-csv3-devops.cfg.erb"
+				variables(
+					:access_log_file_01 => node['logs']['access_log_file_01'],
+					:access_log_stream_01 => node['logs']['access_log_stream_01'],
+					:error_log_file_01 => node['logs']['error_log_file_01'],
+					:error_log_stream_01 => node['logs']['error_log_stream_01'],
+					:access_log_file_02 => node['logs']['access_log_file_02'],
+					:access_log_stream_02 => node['logs']['access_log_stream_02'],
+					:error_log_file_02 => node['logs']['error_log_file_02'],
+					:error_log_stream_02 => node['logs']['error_log_stream_02'],
+					:auth_log_file => node['logs']['auth_log_file'],
+					:auth_log_stream => node['logs']['auth_log_stream']
+				)
 			owner "root"
 			group "root"
 			mode 0644
