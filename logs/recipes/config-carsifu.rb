9b31126d273c758b89053d709d1201a9deb9ddf1
star7kien <che01@PEI-0013.internal.pei-star.com>
2016-05-25 16:55:15 +0800

    modified:   logs/recipes/config-carsifu.rb modified:   logs/templates/default/cw-csv3-beta.cfg.erb new file:   logs/templates/default/cw-csv3-cms-beta.cfg.erb modified:   logs/templates/default/cw-csv3-devops.cfg.erb new file:   logs/templates/default/cw-csv3.cfg.erb
    *CloudWatch Log feature for carsifu

--- logs/recipes/config-carsifu.rb
+++ logs/recipes/config-carsifu.rb
@@ -5,7 +5,7 @@
 #
 
 case node[:opsworks][:stack][:name]
-	when "carsifu_v3-devops"
+	when "carsifu_v3-devops", "carsifu_v3-staging", "carsifu-v3.0"
 		template "/tmp/cwlogs.cfg" do
 			cookbook "logs"
 			source "cw-csv3-devops.cfg.erb"
@@ -29,6 +29,34 @@ case node[:opsworks][:stack][:name]
 		template "/tmp/cwlogs.cfg" do
 			cookbook "logs"
 			source "cw-csv3-beta.cfg.erb"
+			variables(
+					:access_log_file_01 => node['logs']['access_log_file_01'],
+					:access_log_stream_01 => node['logs']['access_log_stream_01'],
+					:error_log_file_01 => node['logs']['error_log_file_01'],
+					:error_log_stream_01 => node['logs']['error_log_stream_01'],
+					:access_log_file_02 => node['logs']['access_log_file_02'],
+					:access_log_stream_02 => node['logs']['access_log_stream_02'],
+					:error_log_file_02 => node['logs']['error_log_file_02'],
+					:error_log_stream_02 => node['logs']['error_log_stream_02'],
+					:access_log_file_03 => node['logs']['access_log_file_03'],
+					:access_log_stream_03 => node['logs']['access_log_stream_03'],
+					:error_log_file_03 => node['logs']['error_log_file_03'],
+					:error_log_stream_03 => node['logs']['error_log_stream_03'],
+					:access_log_file_04 => node['logs']['access_log_file_04'],
+					:access_log_stream_04 => node['logs']['access_log_stream_04'],
+					:error_log_file_04 => node['logs']['error_log_file_04'],
+					:error_log_stream_04 => node['logs']['error_log_stream_04'],
+					:access_log_file_05 => node['logs']['access_log_file_05'],
+					:access_log_stream_05 => node['logs']['access_log_stream_05'],
+					:error_log_file_05 => node['logs']['error_log_file_05'],
+					:error_log_stream_05 => node['logs']['error_log_stream_05'],
+					:access_log_file_06 => node['logs']['access_log_file_06'],
+					:access_log_stream_06 => node['logs']['access_log_stream_06'],
+					:error_log_file_06 => node['logs']['error_log_file_06'],
+					:error_log_stream_06 => node['logs']['error_log_stream_06'],
+					:auth_log_file => node['logs']['auth_log_file'],
+					:auth_log_stream => node['logs']['auth_log_stream']
+				)
 			owner "root"
 			group "root"
 			mode 0644
@@ -37,18 +65,46 @@ case node[:opsworks][:stack][:name]
 		template "/tmp/cwlogs.cfg" do
 			cookbook "logs"
 			source "cw-csv3-cms-beta.cfg.erb"
+			variables(
+					:access_log_file_01 => node['logs']['access_log_file_01'],
+					:access_log_stream_01 => node['logs']['access_log_stream_01'],
+					:error_log_file_01 => node['logs']['error_log_file_01'],
+					:error_log_stream_01 => node['logs']['error_log_stream_01'],
+					:access_log_file_02 => node['logs']['access_log_file_02'],
+					:access_log_stream_02 => node['logs']['access_log_stream_02'],
+					:error_log_file_02 => node['logs']['error_log_file_02'],
+					:error_log_stream_02 => node['logs']['error_log_stream_02'],
+					:access_log_file_03 => node['logs']['access_log_file_03'],
+					:access_log_stream_03 => node['logs']['access_log_stream_03'],
+					:error_log_file_03 => node['logs']['error_log_file_03'],
+					:error_log_stream_03 => node['logs']['error_log_stream_03'],
+					:access_log_file_04 => node['logs']['access_log_file_04'],
+					:access_log_stream_04 => node['logs']['access_log_stream_04'],
+					:error_log_file_04 => node['logs']['error_log_file_04'],
+					:error_log_stream_04 => node['logs']['error_log_stream_04'],
+					:auth_log_file => node['logs']['auth_log_file'],
+					:auth_log_stream => node['logs']['auth_log_stream']
+				)
 			owner "root"
 			group "root"
 			mode 0644
 		end
-	when "carsifu_v3-staging"
+	else
 		template "/tmp/cwlogs.cfg" do
 			cookbook "logs"
-			source "cw-csv3-staging.cfg.erb"
+			source "cw-csv3.cfg.erb"
+				variables(
+					:access_log_file_01 => node['logs']['access_log_file_01'],
+					:access_log_stream_01 => node['logs']['access_log_stream_01'],
+					:error_log_file_01 => node['logs']['error_log_file_01'],
+					:error_log_stream_01 => node['logs']['error_log_stream_01'],
+					:auth_log_file => node['logs']['auth_log_file'],
+					:auth_log_stream => node['logs']['auth_log_stream']
+				)
 			owner "root"
 			group "root"
 			mode 0644
-		end	
+		end
 end
 
 
