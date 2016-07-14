eb57844dec77ef0cf1bd4c2adc110caf504c52a2
star7kien <che01@PEI-0013.internal.pei-star.com>
2016-07-14 17:02:45 +0800

    modified:   my_protectstaging/recipes/carsifu-cms.rb
    *Added line spacing to avoid spacing error.

--- my_protectstaging/recipes/carsifu-cms.rb
+++ my_protectstaging/recipes/carsifu-cms.rb
@@ -7,7 +7,7 @@
 node[:deploy].each do |application, deploy|
 
 	execute "protect dev qa staging environment" do
-		command %Q[echo "# BEGIN Basic Access Authentication on protecting dev, qa, beta, staging stacks\n    AuthName '"'Restricted Area'"'\n    AuthType Basic\n    AuthUserFile /etc/apache2/.htpasswd\n    AuthGroupFile /dev/null\n    require valid-user" >> #{deploy[:deploy_to]}/current/.htaccess]
+		command %Q[echo "\n# BEGIN Basic Access Authentication on protecting dev, qa, beta, staging stacks\n    AuthName '"'Restricted Area'"'\n    AuthType Basic\n    AuthUserFile /etc/apache2/.htpasswd\n    AuthGroupFile /dev/null\n    require valid-user" >> #{deploy[:deploy_to]}/current/.htaccess]
 		action :run
 	end
 end
