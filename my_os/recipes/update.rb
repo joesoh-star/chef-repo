39302c11e6a69ae8d80fe8b96861b751f1f15eab
star7kien <che01@PEI-0013.internal.pei-star.com>
2016-08-09 14:11:44 +0800

    modified:   my_os/recipes/update.rb
    *Include rhel platform family

--- my_os/recipes/update.rb
+++ my_os/recipes/update.rb
@@ -2,7 +2,16 @@
 # Update OS Patches
 #
 
-execute "update os" do
-  command "apt-get update && apt-get -y dist-upgrade"
-  action :run
+if platform_family?('debian')
+	execute "update os" do
+		command "apt-get update && apt-get -y dist-upgrade"
+	action :run
+	end
+end
+
+if platform_family?('rhel')
+	execute "update os" do
+		command "yum -y update"
+	action :run
+	end
 end
