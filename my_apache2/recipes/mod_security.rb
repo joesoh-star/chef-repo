0777fcb4772016ae7f0a6af77b5bffffe681b977
star7kien <kienonn.chee@thestar.com.my>
2016-11-22 17:14:01 +0800

    modified:   apache2/templates/ubuntu-14.04/security.conf.erb *Fixed syntax error
    new file:   my_apache2/recipes/mod_security.rb
    *mod_security for apache

---
+++ my_apache2/recipes/mod_security.rb
@@ -0,0 +1,13 @@
+case node[:platform_family]
+when 'debian'
+  package 'libapache2-mod-security2' do
+    action :install
+    retries 3
+    retry_delay 5
+  end
+when 'rhel'
+  package 'mod_security' do
+    action :install
+    retries 3
+    retry_delay 5
+  end
