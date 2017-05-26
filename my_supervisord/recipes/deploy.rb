8593ffdda4baba06436793e38f12927c24a87fc4
star7kien <kienonn.chee@thestar.com.my>
2017-05-26 11:50:13 +0800

    *Added numprocs=20 to the config

--- my_supervisord/recipes/deploy.rb
+++ my_supervisord/recipes/deploy.rb
@@ -8,20 +8,20 @@ node[:deploy].each do |application, deploy|
 	end
 
 	execute "insert-conf" do
-		command %Q[echo "[program:#{deploy[:domains].first}]\ncommand=php #{deploy[:deploy_to]}/current/artisan queue:work --sleep=3 --tries=3 --daemon\nautostart=true\nautorestart=true\nstderr_logfile=/var/log/supervisor/#{deploy[:domains].first}.err.log\nstdout_logfile=/var/log/supervisor/#{deploy[:domains].first}.out.log\nuser=www-data\n" > /etc/supervisor/conf.d/#{deploy[:domains].first}.conf]
+		command %Q[echo "[program:#{deploy[:domains].first}]\ncommand=php #{deploy[:deploy_to]}/current/artisan queue:work --sleep=3 --tries=3 --daemon\nautostart=true\nautorestart=true\nnumprocs=20\nprocess_name=%(program_name)s_%(process_num)02d\nstderr_logfile=/var/log/supervisor/#{deploy[:domains].first}.err.log\nstdout_logfile=/var/log/supervisor/#{deploy[:domains].first}.out.log\nuser=www-data\n" > /etc/supervisor/conf.d/#{deploy[:domains].first}.conf]
 		action :run
 	end
-	
+
 	execute "supervisor-reread-config" do
 		command "supervisorctl reread"
 		action :run
 	end
-	
+
 	execute "supervisor-update-config" do
 		command "supervisorctl update"
 		action :run
 	end
-	
+
 	execute "restart-daemon" do
 		command %Q[php #{deploy[:deploy_to]}/current/artisan queue:restart]
 		action :run
