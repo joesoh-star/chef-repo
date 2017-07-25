f409639dab7ef2455eadf5e489499b55138abe66
K <k@ip-192-168-22-111.ap-southeast-1.compute.internal>
2017-07-25 10:59:19 +0800

    *Move out supervisor parameters to opsworks custom json

--- my_supervisord/recipes/deploy.rb
+++ my_supervisord/recipes/deploy.rb
@@ -2,14 +2,25 @@
 #For laravel job server only
 
 node[:deploy].each do |application, deploy|
-	execute "create-conf" do
-		command %Q[touch /etc/supervisor/conf.d/#{deploy[:domains].first}.conf]
-		action :run
-	end
+#	execute "create-conf" do
+#		command %Q[touch /etc/supervisor/conf.d/#{deploy[:domains].first}.conf]
+#		action :run
+#	end
 
-	execute "insert-conf" do
-		command %Q[echo "[program:#{deploy[:domains].first}]\ncommand=php #{deploy[:deploy_to]}/current/artisan queue:work --sleep=3 --tries=3 --daemon\nautostart=true\nautorestart=true\nnumprocs=5\nprocess_name=%(program_name)s_%(process_num)02d\nstderr_logfile=/var/log/supervisor/#{deploy[:domains].first}.err.log\nstdout_logfile=/var/log/supervisor/#{deploy[:domains].first}.out.log\nuser=www-data\n" > /etc/supervisor/conf.d/#{deploy[:domains].first}.conf]
-		action :run
+#	execute "insert-conf" do
+#		command %Q[echo "[program:#{deploy[:domains].first}]\ncommand=php #{deploy[:deploy_to]}/current/artisan queue:work --sleep=3 --tries=3 --daemon\nautostart=true\nautorestart=true\nnumprocs=5\nprocess_name=%(program_name)s_%(process_num)02d\nstderr_logfile=/var/log/supervisor/#{deploy[:domains].first}.err.log\nstdout_logfile=/var/log/supervisor/#{deploy[:domains].first}.out.log\nuser=www-data\n" > /etc/supervisor/conf.d/#{deploy[:domains].first}.conf]
+#		action :run
+#	end
+	app_name = "#{deploy[:domains].first}"
+
+	template "/etc/supervisor/conf.d/#{app_name}.conf]" do
+		source "supervisor-app.conf.erb"
+		variables (
+			:commandpath => node['my_supervisord']['commandpath'],
+			:numprocs => node['my_supervisord']['numprocs'],
+			:appname => "#{app_name}",
+			:apppath => "#{deploy[:deploy_to]}"
+		)
 	end
 
 	execute "supervisor-reread-config" do
