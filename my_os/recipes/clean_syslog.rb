89c9746607f871b8b8fe7e7f818a03771241ce59
K <k@ip-192-168-22-111.ap-southeast-1.compute.internal>
2017-08-08 17:24:05 +0800

    * Fixed Syntax

--- my_os/recipes/clean_syslog.rb
+++ my_os/recipes/clean_syslog.rb
@@ -4,14 +4,14 @@
 
 if platform_family?('debian')
 	execute "clean-syslog" do
-		command "cat /dev/null /var/log/syslog"
+		command "cat /dev/null > /var/log/syslog"
 	action :run
 	end
 end
 
 if platform_family?('rhel')
 	execute "clean-syslog" do
-		command "cat /dev/null /var/log/message"
+		command "cat /dev/null > /var/log/message"
 	action :run
 	end
 end
