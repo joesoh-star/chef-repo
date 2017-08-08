c83fe85a4e19248489db4ae045a4024e095b694c
K <k@ip-192-168-22-111.ap-southeast-1.compute.internal>
2017-08-08 17:14:16 +0800

    *Added clean-syslog recipe

---
+++ my_os/recipes/clean_syslog.rb
@@ -0,0 +1,17 @@
+#
+# Clean Syslog
+#
+
+if platform_family?('debian')
+	execute "clean-syslog" do
+		command "cat /dev/null /var/log/syslog"
+	action :run
+	end
+end
+
+if platform_family?('rhel')
+	execute "clean-syslog" do
+		command "cat /dev/null /var/log/message"
+	action :run
+	end
+end
